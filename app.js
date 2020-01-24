575530a5f4c034c21f5640df0c30764cbe869386
hbsoftco <hossein.bajan@gmail.com>
2020-01-24 21:56:45 +0330

    Update

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
+// Initialize the app
+const app = express();
+// Defining the PORT
+const PORT = process.env.PORT || 5000;
+
+// Middllwares
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+
+// Routes
+app.use('/users', require('./routes/users'))
+
+// Start the server
+app.listen(PORT, () => {
+    console.log('Server run at http://localhost:5000');
+});
---
+++ controllers/users.js
@@ -0,0 +1,19 @@
+module.exports = {
+    signUp: async (req, res, next) => {
+        console.log('SignUp UserController');
+
+        // try {
+
+        // } catch (error) {
+        //     next(error);
+        // }
+    },
+
+    signIn: async (req, res, next) => {
+        console.log('signIn UserController');
+    },
+
+    secret: async (req, res, next) => {
+        console.log('secret UserController');
+    }
+}
